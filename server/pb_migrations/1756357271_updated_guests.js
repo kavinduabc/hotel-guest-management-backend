c47a8ca106d9d167de0fa8aefefeffd22d5aa1bb
kavinduabc <piumalranepura2002@gmail.com>
2025-09-01 10:38:44 +0530

    feat : complete full backend

---
+++ server/pb_migrations/1756357271_updated_guests.js
@@ -0,0 +1,180 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_2047001084")
+
+  // add field
+  collection.fields.addAt(6, new Field({
+    "exceptDomains": [],
+    "hidden": false,
+    "id": "email3885137012",
+    "name": "email",
+    "onlyDomains": [],
+    "presentable": false,
+    "required": true,
+    "system": false,
+    "type": "email"
+  }))
+
+  // update field
+  collection.fields.addAt(1, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text2849095986",
+    "max": 0,
+    "min": 0,
+    "name": "first_name",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": true,
+    "system": false,
+    "type": "text"
+  }))
+
+  // update field
+  collection.fields.addAt(2, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text3356015194",
+    "max": 0,
+    "min": 0,
+    "name": "last_name",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": true,
+    "system": false,
+    "type": "text"
+  }))
+
+  // update field
+  collection.fields.addAt(3, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text1146066909",
+    "max": 0,
+    "min": 0,
+    "name": "phone",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": true,
+    "system": false,
+    "type": "text"
+  }))
+
+  // update field
+  collection.fields.addAt(4, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text223244161",
+    "max": 0,
+    "min": 0,
+    "name": "address",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": true,
+    "system": false,
+    "type": "text"
+  }))
+
+  // update field
+  collection.fields.addAt(5, new Field({
+    "hidden": false,
+    "id": "date1089581607",
+    "max": "",
+    "min": "",
+    "name": "date_of_birth",
+    "presentable": false,
+    "required": true,
+    "system": false,
+    "type": "date"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_2047001084")
+
+  // remove field
+  collection.fields.removeById("email3885137012")
+
+  // update field
+  collection.fields.addAt(1, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text2849095986",
+    "max": 0,
+    "min": 0,
+    "name": "first_name",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  // update field
+  collection.fields.addAt(2, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text3356015194",
+    "max": 0,
+    "min": 0,
+    "name": "last_name",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  // update field
+  collection.fields.addAt(3, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text1146066909",
+    "max": 0,
+    "min": 0,
+    "name": "phone",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  // update field
+  collection.fields.addAt(4, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text223244161",
+    "max": 0,
+    "min": 0,
+    "name": "address",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  // update field
+  collection.fields.addAt(5, new Field({
+    "hidden": false,
+    "id": "date1089581607",
+    "max": "",
+    "min": "",
+    "name": "date_of_birth",
+    "presentable": false,
+    "required": false,
+    "system": false,
+    "type": "date"
+  }))
+
+  return app.save(collection)
+})
